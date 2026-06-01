65e9f978cc9f843c2d2a2c7f30ecba52e9f183f2
wiryaptr <weryoputro07@gmail.com>
2026-06-01 18:22:13 +0700

    Fix on del, sum, min, div

--- script.js
+++ script.js
@@ -6,7 +6,27 @@ const buttons = document.querySelectorAll(".btn");
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
         const value = button.textContent;
-        expression += value;
-        display.value = expression;
+
+        if (value === "C"){
+            expression = "";
+            display.value = "";
+        }
+        else if (value === "⌫"){
+            expression = expression.slice(0, -1);
+            display.value = expression;
+        }
+        else if (value === "="){
+            try {
+                expression = eval(expression);
+                display.value = expression;
+            } catch {
+                display.value = "Error";
+                expression = "";
+            }
+        }
+        else {
+            expression += value;
+            display.value = expression;
+        }
     });
 });
