453742c09706011cee1eca824d27cfbc2fb5e478
wiryaptr <weryoputro07@gmail.com>
2026-06-02 19:57:37 +0700

    Add feature % and fix bug

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let expression = "";
 
+const operators = ["+", "-", "*", "/"];
+
 const display = document.getElementById("display");
 const buttons = document.querySelectorAll(".btn");
 
@@ -16,6 +18,7 @@ buttons.forEach((button) => {
             display.value = expression;
         }
         else if (value === "="){
+            if (expression === "") return;
             try {
                 expression = eval(expression);
                 display.value = expression;
@@ -25,8 +28,52 @@ buttons.forEach((button) => {
             }
         }
         else {
+            const lastChar = expression.slice(-1);
+
+            if (operators.includes(lastChar) && operators.includes(value)) {
+                return;
+            }
+
+            if (value === "."){
+                const lastNumber = expression.split(/[+\-*/]/).pop();
+                if (lastNumber.includes(".")) {
+                    return;
+                }
+            }
+            else if (value === "%"){
+                expression = String(Number(expression)/100);
+                display.value = expression;
+                return;
+            }
             expression += value;
             display.value = expression;
         }
     });
+});
+
+document.addEventListener("keydown", (event) => {
+    const key = event.key;
+
+    if ("0123456789+-*/.".includes(key)) {
+        const btn =
+            [...buttons].find(
+                button => button.textContent === key
+            );
+        if (btn) btn.click();
+    }
+
+    if (key === "Enter"){
+        const equalBtn = document.querySelector(".equal");
+        equalBtn.click();
+    }
+
+    if (key === "Backspace"){
+        const deleteBtn = [...buttons].find(button => button.textContent === "⌫");
+        deleteBtn.click();
+    }
+
+    if (key === "Escape"){
+        const clearBtn = document.querySelector(".clear");
+        clearBtn.click();
+    }
 });
